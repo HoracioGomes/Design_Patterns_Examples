b876ca57be6c0903ffff8d29437e8356da090440
Horacio <oicaroh64@gmail.com>
2020-08-03 18:18:17 -0300

    Implementacao Strategy

---
+++ src/strategy/ICMS.java
@@ -0,0 +1,12 @@
+package strategy;
+
+public class ICMS implements Imposto {
+
+	@Override
+	public double calcula(Orcamento orcamento) {
+		// TODO Auto-generated method stub
+		return orcamento.getValor() * 0.1;
+
+	}
+
+}
